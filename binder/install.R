52b627ab3f13d112f594b4e7930a1749d738d582
Arnim Bleier <arnim.bleier@gmail.com>
2019-03-18 17:01:04 +0100

    add blavaan ...

--- binder/install.R
+++ binder/install.R
@@ -1,2 +1,7 @@
 install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies=TRUE)
 install.packages("tidyverse", repos = "https://cloud.r-project.org/", dependencies=TRUE)
+
+install.packages("psych", repos = "https://cloud.r-project.org/", dependencies=TRUE)
+install.packages("blavaan", repos = "https://cloud.r-project.org/", dependencies=TRUE)
+install.packages("rjags", repos = "https://cloud.r-project.org/", dependencies=TRUE)
+install.packages("runjags", repos = "https://cloud.r-project.org/", dependencies=TRUE)
